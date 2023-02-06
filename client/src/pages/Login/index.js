28988d2b148c457fa3ecd763d389b8d05d9670b4
Pedro <peedrofranq96@gmail.com>
2023-02-06 12:39:48 -0300

    inicio da tela de Login

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,7 +1,24 @@
 import React from "react";
 import './styles.css';
 
+import logoImage from '../../assets/logo.svg'
+import padlock from '../../assets/padlock.png'
+
 
 export default function Login(){
-    return(<h1>Welcome</h1>);
+    return(
+        <div className="login-container">   
+                <section className="form">
+                <img src={logoImage} alt="Erudio Logo"/>
+                <form>
+                    <h1>Access your account</h1>
+                    <input placeholder="UserName"/>
+                    <input type="password" placeholder="Password"/>
+
+                    <button type="submit">Login</button>
+                </form>
+            </section>
+            <img src={padlock} alt="Login"/>
+        </div>
+    );
 }
