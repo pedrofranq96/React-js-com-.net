63a8ef5259e2ed28214158761b8667f223c31194
Pedro <peedrofranq96@gmail.com>
2023-02-06 13:53:28 -0300

    configurando a rota e adicionando a tela de Books

---
+++ client/src/pages/Books/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import './styles.css';
+
+export default function Books(){
+    return <h1>Books</h1>
+}
--- client/src/routes.js
+++ client/src/routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Login from './pages/Login';
+import Books from './pages/Books';
 
 export default function Routes(){
     return(
         <BrowserRouter>
             <Switch>
-                <Route path="/" component={Login}/>
+                <Route path="/" exact component={Login}/>
+                <Route path="/books" component={Books}/>
             </Switch>
         </BrowserRouter>
     );
