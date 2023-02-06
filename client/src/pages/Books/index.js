1bf52d7d420c669732fb7a7115a2273a2e21a936
Pedro <peedrofranq96@gmail.com>
2023-02-06 14:19:00 -0300

    Configs da tela de books

--- client/src/pages/Books/index.js
+++ client/src/pages/Books/index.js
@@ -1,7 +1,19 @@
 import React from "react";
+import {Link} from 'react-router-dom';
+import {FiPower} from 'react-icons/fi'
 
+import logoImage from '../../assets/logo.svg'
 import './styles.css';
 
 export default function Books(){
-    return <h1>Books</h1>
+    return <div className="book-container">
+        <header>
+            <img src={logoImage} alt="Erudio"/>
+            <span>Bem vindo, <strong>Pedro</strong>!</span>
+            <Link className="button" to="book/new">Add New Book</Link>
+            <button type="button">
+                <FiPower size={18} color="#251FC5"/>
+            </button>
+        </header>
+    </div>
 }
